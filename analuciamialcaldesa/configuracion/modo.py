bbcbeb6e94e4f91c094e4256c41a90540270db5e
Ajerhy <34432038+Ajerhy@users.noreply.github.com>
2021-02-15 15:17:51 -0400

    add

--- analuciamialcaldesa/configuracion/modo.py
+++ analuciamialcaldesa/configuracion/modo.py
@@ -8,8 +8,8 @@ try:
         print("Usando la configuración de PRODUCCIÓN.")
     else:
         DEBUG = True
-        #ALLOWED_HOSTS = ['*']
-        ALLOWED_HOSTS = ['analuciamialcaldesa.herokuapp.com']
+        ALLOWED_HOSTS = ['*']
+        #ALLOWED_HOSTS = ['analuciamialcaldesa.herokuapp.com']
         SECRET_KEY = '0oc305u3geb+wlwy!quqdxt$d0_*!9#7f1lm#a+wzh3*wk1+oq'
         DEBUG_PROPAGATE_EXCEPTIONS = True
 except:
